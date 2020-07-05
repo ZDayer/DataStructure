cdc59c1cb76d1883815fa83b432c3a9a11078bf7
ZDayer <zhaoyangofemail@163.com>
2020-07-05 21:04:40 +0800

    添加String

---
+++ DataStructure/串/String.c
@@ -0,0 +1,9 @@
+//
+//  String.c
+//  DataStructure
+//
+//  Created by 大洋 on 2020/7/5.
+//  Copyright © 2020 Dayer. All rights reserved.
+//
+
+#include "String.h"
---
+++ DataStructure/串/String.h
@@ -0,0 +1,32 @@
+//
+//  String.h
+//  DataStructure
+//
+//  Created by 大洋 on 2020/7/5.
+//  Copyright © 2020 Dayer. All rights reserved.
+//
+
+#ifndef String_h
+#define String_h
+
+#include <stdio.h>
+
+
+/*
+ 串中的元素仅由一个字符组成, 相邻元素具有前驱和后继关系
+ 
+ StrAssign(T, *chars): 生成一个其值等于字符串常量chars 的串 T
+ StrCopy(T, S)
+ ClearString(S)
+ StringEmpty(S)
+ StrLength(S)
+ StrCompare(S,T) 对比大小
+ Concat(T, S1, S2) 用 T 返回由 S1和 S2 联接而成的新串
+ SubStrin(Sub,S,pos,len)  position
+ Index(S,T,pos) 串 S和 T 存在, T 是非空串, 1<= pos <= StrLength(S), 若主串 S 中存在和串 T 值相同的子串, 则返回它在主串 S 中第 pos 个字符之后第一次出现的位置, 否则返回 0
+ Replace(S,T,V) 串 S, T 和 V 存在, T 是非空串, 用 V 替换主串 S 中出现的所有与 T 相等的不重复子串
+ StrInsert(S,pos,T)
+ StrDelete(S,pos,len) 
+ */
+
+#endif /* String_h */
