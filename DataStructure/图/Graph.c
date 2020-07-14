0e3df9f8cf25dd4b00a60a17af14fd4c0629adfc
ZDayer <zhaoyangofemail@163.com>
2020-07-14 23:20:09 +0800

    添加graph operation

--- DataStructure/图/Graph.c
+++ DataStructure/图/Graph.c
@@ -7,3 +7,24 @@
 //
 
 #include "Graph.h"
+
+
+void CreateGraph(MGraph *G) {
+    printf("输入顶点数和边数:\n");
+    scanf("%d,%d",&G->numVertexes, &G->numEdges); // 输入顶点数和边数
+    for (int i = 0; i < G->numVertexes; i++) {
+        scanf("%c",&G->vexs[i]);
+    }
+    for (int i = 0; i < G->numVertexes; i++) {
+        for (int j = 0; j < G->numVertexes; j++) {
+            G->arc[i][j] = INFINITY; // 初始化
+        }
+    }
+    for (int k = 0; k < G->numEdges; k++) {
+        printf("输入边(vi, Vj)上的下标 i,下标j 和权值 w:\n");
+        int i, j, w;
+        scanf("%d,%d,%d", &i, &j, &w);
+        G->arc[i][j] = w;
+        G->arc[j][i] = G->arc[i][j];  // 无向图, 矩阵对称
+    }
+}
--- DataStructure/图/Graph.h
+++ DataStructure/图/Graph.h
@@ -11,4 +11,21 @@
 
 #include <stdio.h>
 
+// 邻接矩阵
+typedef char VertexType;    // 顶点类型, 自定义
+typedef int EdgeType;       // 权值类型, 自定义
+#define MAXVEX 100
+#define INFINITY 65535      // 65535是计算机16位二进制最大数, 代替极限值
+
+typedef struct {
+    VertexType vexs[MAXVEX]; // 顶点表
+    EdgeType arc[MAXVEX][MAXVEX]; // 邻接矩阵, 也可以看做边表
+    int numVertexes, numEdges; // 顶点数, 边树数
+}MGraph;
+
+
+// 构造图, 就是向顶点表和边表填入数据的过程
+
+// 无向网图
+void CreateGraph(MGraph *G);
 #endif /* Graph_h */
